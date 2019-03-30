9cd845318a80a8d0e5bc133e47aa9dd2c913b773
deganii <idegani@mit.edu>
2019-03-30 12:19:37 -0400

    fix prediction model name

--- src/run.py
+++ src/run.py
@@ -6,10 +6,10 @@ from src.processing.predict import prediction
 from src.data.loader import DataLoader
 import keras.layers.advanced_activations as A
 
-train_unet('nucleus-4dirs', dataset='0129-2dirs',
-           num_layers=6, filter_size=3,
-           learn_rate=1e-4, conv_depth=32, epochs=25,
-           records=-1, batch_size=16, activation=A.PReLU,
-           advanced_activations=True, last_activation=A.PReLU)
+# train_unet('nucleus-4dirs', dataset='0129-2dirs',
+#            num_layers=6, filter_size=3,
+#            learn_rate=1e-4, conv_depth=32, epochs=25,
+#            records=-1, batch_size=16, activation=A.PReLU,
+#            advanced_activations=True, last_activation=A.PReLU)
 data, label = DataLoader.load_testing(records=-1, dataset='0129-2dirs')
-ssim = prediction('nucleus-4dirs', data, label)
+ssim = prediction('unet_6-3_mse_nucleus-4dirs', data, label)
