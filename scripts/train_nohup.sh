0b7fb8781a018dc3e61c02262b0c1aa507611a4c
deganii <idegani@mit.edu>
2019-03-29 23:07:36 -0400

    minor cleanup

--- scripts/train_nohup.sh
+++ scripts/train_nohup.sh
@@ -4,8 +4,6 @@ BASE=/home/ubuntu/nucleus-prediction
 MODEL_NAME=$1
 PROC_DIR=$BASE/src
 LOG_DIR=$BASE/logs/$MODEL_NAME
-
-mkdir $BASE/logs
-mkdir $BASE/logs/$MODEL_NAME
+mkdir -p $LOG_DIR
 
 cd $PROC_DIR && nohup python3.5 run.py > $LOG_DIR/$MODEL_NAME.out 2> $LOG_DIR/$MODEL_NAME.err < /dev/null &
