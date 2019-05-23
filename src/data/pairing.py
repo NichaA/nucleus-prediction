7f68441f8c87e054ba2c3b7dee30fc09ec267f48
deganii <idegani@mit.edu>
2019-05-23 10:47:27 -0400

    first cut of working pairings

--- src/data/pairing.py
+++ src/data/pairing.py
@@ -2,13 +2,16 @@ import csv
 import os
 import glob
 import re
+import ntpath
+
+PAIRING_FILE = 'pairing.csv'
 
 # id, filename, pair_id
 def generate_pairings(dir, debug=False):
-    with open(os.path.join(dir, 'pairing.csv'), mode='w') as csv_file:
+    with open(os.path.join(dir, PAIRING_FILE), mode='w', newline='') as csv_file:
         # returns a list of dapi/trans tuples for a given folder
         input_files = glob.glob(dir + '*.trans.tif')
-        num_input_files = len(input_files)
+        # num_input_files = len(input_files)
         # print("Num input files: {0}".format(num_input_files))
         pruned_input_files = []
         trans_pattern = re.compile(r"_[A-Za-z0-9]+_\d+ms\.trans\.tif")
@@ -16,32 +19,47 @@ def generate_pairings(dir, debug=False):
             dapi_file = trans_file.replace('trans.tif', 'dapi.tif')
             if os.path.isfile(dapi_file):
                 pruned_input_files.append((trans_file, dapi_file))
-            else: # next try to remove the exposure time from trans...
-                subbed = trans_pattern.sub('*.dapi.tif', trans_file)
-                matching_dapis = glob.glob(subbed)
-                if len(matching_dapis) > 0:
-                    pruned_input_files.append((trans_file, matching_dapis[0]))
+            else: # go backwards until we find the first metching dapi
+                for i in range(len(dapi_file),-1,-1):
+                    subbed = dapi_file[:i] + '*.dapi.tif' #trans_pattern.sub('*.dapi.tif', trans_file)
+                    matching_dapis = glob.glob(subbed)
+                    if len(matching_dapis) > 0:
+                        pruned_input_files.append((trans_file, matching_dapis[0]))
+                        break;
+        # serialize the pairs to pairings.csv
+        id = 0
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['id', 'filename', 'pair_id'])
+        seen_files = set()
+        for trans,dapi in pruned_input_files:
+            csv_writer.writerow([id, ntpath.basename(trans), id+1])
+            csv_writer.writerow([id+1, ntpath.basename(dapi), None])
+            seen_files.add(ntpath.basename(trans))
+            seen_files.add(ntpath.basename(dapi))
+            id = id + 2
+
+        # write the unpaired files:
+        for file in glob.glob(dir + '*.*'):
+            if file not in seen_files and ntpath.basename(file) != PAIRING_FILE:
+                csv_writer.writerow([id, ntpath.basename(file), None])
+                id = id + 1
+
         if debug:
-            print("Num input pairs in {0}: {1}\n\n".format(os.path.basename(folder_name), len(pruned_input_files)))
+            print("Num input pairs in {0}: {1}\n\n".format(os.path.basename(dir), len(pruned_input_files)))
             # print("First 2 input file pairs: \n\n{0}\n\n{1}".format(pruned_input_files[0], pruned_input_files[1]))
         return pruned_input_files
 
-        fieldnames = ['', '']
 
-
-# generates list of filename tuples from p
+# generates list of filename tuples from paring.csv
 def get_pairings(dir):
     pairs = []
-    with open('employee_birthday.txt') as csv_file:
+    with open(os.path.join(dir, PAIRING_FILE),'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         id_dict = {}
         headers = next(csv_reader)
         for row in csv_reader:
             id_dict[row[0]] = row
         for id,row in id_dict.items():
-            if row[2] is not None:
+            if row[2]:
                 pairs.append((row[1], id_dict[row[2]][1]))
     return pairs
-
-
-generate_pairings()
--- src/gen.py
+++ src/gen.py
@@ -3,9 +3,11 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from src.data.nucleus import NucleusDataGenerator
+from src.data.pairing import *
 
 # folders for this training session
-base_folder = "/home/ubuntu/Dropbox/Voldman_Group/ML with Cell Images/Images/"
+# base_folder = "/home/ubuntu/Dropbox/Voldman_Group/ML with Cell Images/Images/"
+base_folder = '../data/'
 
 folders = [
     base_folder + "01292018_HUVEC(5x_10x)_edgeAndFlat/HUVECs10x_01292018_flat/",
@@ -18,10 +20,12 @@ folders = [
     # base_folder + "",
 ]
 
-NucleusDataGenerator.generateImages(
-    set_name='nucleus-4dirs',
-    input_folder=folders,
-    output_folder='../data/')
-NucleusDataGenerator.partitionTrainingAndTestSet(set_name='nucleus-4dirs')
+# NucleusDataGenerator.generateImages(
+#     set_name='nucleus-4dirs',
+#     input_folder=folders,
+#     output_folder='../data/')
+# NucleusDataGenerator.partitionTrainingAndTestSet(set_name='nucleus-4dirs')
 
-
+for folder in folders:
+    generate_pairings(folder)
+    print(get_pairings(folder))
